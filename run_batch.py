13ac7051d6c3863fe7d0ac5fad65409894c00c39
lizuoyue <li.zuoyue@foxmail.com>
2019-10-09 22:45:58 +0200

    a

--- run_batch.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-target = '../dataset/L2R_Bicycle'
-os.makedirs(target, exist_ok = True)
-
-for item in glob.glob('../Replica-Dataset/replica_v1/*'):
-	print(item + '/mesh.ply')
---
+++ run_batch.sh
@@ -0,0 +1,8 @@
+g++ -std=c++11 sample_points.cpp -o sample_points -lpthread
+for MESH_FOLDER in $(ls '../Replica-Dataset/replica_v1/*')
+do
+	MESH_FILE=${MESH_FOLDER}"/mesh.ply"
+	SCENE_NAME=$(basename ${MESH_FOLDER})
+	mkdir ${SCENE_NAME}
+	./sample_points -f ${MESH_FILE} -d 5000 -o ${SCENE_NAME} -p 0.05 -v 10
+done
