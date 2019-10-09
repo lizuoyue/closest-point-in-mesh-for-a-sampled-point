cc3e24168879be067ad3b3567819cc57c1693874
lizuoyue <li.zuoyue@foxmail.com>
2019-10-09 23:07:35 +0200

    a

--- run_batch.sh
+++ run_batch.sh
@@ -2,10 +2,12 @@ g++ -std=c++11 sample_points.cpp -o sample_points -lpthread
 MESH_ROOT="../Replica-Dataset/replica_v1/"
 for SCENE_NAME in $(ls ${MESH_ROOT})
 do
+	SCENE_NAME="hotel_0"
 	MESH_FILE=${MESH_ROOT}${SCENE_NAME}"/mesh.ply"
-	mkdir -p ${SCENE_NAME}
+	mkdir -p "scene_"${SCENE_NAME}
 	echo "==========================="
 	echo ${SCENE_NAME}
-	./sample_points -f ${MESH_FILE} -d 3000 -o ${SCENE_NAME} -p 0.05 -v 10
+	./sample_points -f ${MESH_FILE} -d 3000 -o "scene_"${SCENE_NAME} -p 0.05 -v 10
 	echo "==========================="
+	break
 done
