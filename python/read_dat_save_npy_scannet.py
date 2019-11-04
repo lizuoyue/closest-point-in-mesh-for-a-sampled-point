7b0fb7fd85e387c3c46585246dff0ce825d58a28
lizuoyue <li.zuoyue@foxmail.com>
2019-11-04 12:45:52 +0100

    a

--- python/read_dat_save_npy_scannet.py
+++ python/read_dat_save_npy_scannet.py
@@ -14,15 +14,6 @@ def l2dist(p1, p2):
 	diff = p1 - p2
 	return np.sqrt(diff.dot(diff))
 
-def common(p, li):
-	l = np.array([[item[0], item[1], item[2], 1] for item in li])
-	assert(almost0(np.linalg.det(l)))
-	for i in range(4):
-		lc = l.copy()
-		lc[i, :3] = p
-		assert(almost0(np.linalg.det(lc)))
-	return
-
 folders = glob.glob('../scannet/scene_*')
 for folder in folders:
 	points = np.fromfile(folder + '/point_coordinate.dat', np.float32).reshape((-1, 3))
@@ -48,7 +39,6 @@ for folder in folders:
 
 	for i in tqdm.tqdm(range(points.shape[0])):
 		assert(almost0(dist[i] - l2dist(points[i], c_points[i])))
-		common(c_points[i], mesh_vertex[mesh_face[fid[i]][0]])
 		ins_id[i] = mesh_face[fid[i]][1]
 
 	d = {
@@ -59,7 +49,7 @@ for folder in folders:
 		'nearest_instance_index': ins_id,
 	}
 
-	np.save('%s.npy' % scene_name, d) 
+	np.save('%s.npy' % scene_name, d)
 
 	# Load
 	# d = np.load('%s.npy' % scene_name).item()
